666670cf4e296e1ec5b8a17f478edbce5302621a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-06-06 16:38:02 +0100

    Add Color files

---
+++ Include/Ishiko/Types/Color.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Types/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TYPES_COLOR_H_
+#define _ISHIKO_TYPES_COLOR_H_
+
+#endif
