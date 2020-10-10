a4d34a21f4ed06af30f2e9d069f920a6e9cb4406
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-10 19:21:55 +0100

    Add Color class and tests (#1)

---
+++ Tests/Source/ColorTests.h
@@ -0,0 +1,21 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Types/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TYPES_TEST_COLORTESTS_H_
+#define _ISHIKO_TYPES_TEST_COLORTESTS_H_
+
+#include "Ishiko/TestFramework/TestFrameworkCore.h"
+
+class ColorTests : public Ishiko::Tests::TestSequence
+{
+public:
+    ColorTests(const Ishiko::Tests::TestNumber& number, const Ishiko::Tests::TestEnvironment& environment);
+
+private:
+    static void ConstructorTest1(Ishiko::Tests::Test& test);
+};
+
+#endif
