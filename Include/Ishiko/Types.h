ed5b6f1fc19b4f1a1391e3a8c09e16ad0fd2a2a4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-24 19:17:25 +0100

    Add files

---
+++ Include/Ishiko/Types.h
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Types/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TYPES_H_
+#define _ISHIKO_TYPES_H_
+
+#include "Types/Color.h"
+#include "Types/Point2D.h"
+
+#include "Types/linkoptions.h"
+
+#endif
---
+++ Include/Ishiko/Types/Point2D.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Types/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TYPES_POINT2D_H_
+#define _ISHIKO_TYPES_POINT2D_H_
+
+#endif
