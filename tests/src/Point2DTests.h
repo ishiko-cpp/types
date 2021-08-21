3baa595816bd74a8d75bd03ec95ce613aea0eb4b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-21 20:49:41 +0100

    Maintenance (#27)

--- tests/src/Point2DTests.h
+++ tests/src/Point2DTests.h
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Types/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/types/blob/main/LICENSE.txt
 */
 
 #ifndef _ISHIKO_TYPES_TESTS_POINT2DTESTS_H_
