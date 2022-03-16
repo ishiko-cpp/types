1d303a4945e096f7fd94bbed7fed98102dd4a99e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-16 17:27:50 +0000

    Implement and test URL accessors

--- include/Ishiko/Types/URL.hpp
+++ include/Ishiko/Types/URL.hpp
@@ -7,6 +7,7 @@
 #ifndef _ISHIKO_CPP_TYPES_URL_HPP_
 #define _ISHIKO_CPP_TYPES_URL_HPP_
 
+#include <boost/optional.hpp>
 #include <string>
 
 namespace Ishiko
@@ -16,6 +17,13 @@ class URL
 {
 public:
     URL(const std::string& url);
+
+    std::string scheme() const;
+    boost::optional<std::string> authority() const;
+    boost::optional<std::string> host() const;
+    std::string path() const;
+    boost::optional<std::string> query() const;
+    boost::optional<std::string> fragment() const;
 
     bool operator==(const std::string& other) const noexcept;
     bool operator!=(const std::string& other) const noexcept;
