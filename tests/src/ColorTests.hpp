28614c0fb135b57ddf09138ca39756192e22dab3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 16:19:05 +0100

    Fix test code build (#40)

--- tests/src/ColorTests.hpp
+++ tests/src/ColorTests.hpp
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_TYPES_TESTS_COLORTESTS_HPP_
 #define _ISHIKO_CPP_TYPES_TESTS_COLORTESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class ColorTests : public Ishiko::TestSequence
 {
